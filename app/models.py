01de003646f774001ee4316dba435094128407c3
sancara <santiago_caraballo@hotmail.com>
2024-05-30 11:20:50 -0300

    add timestamp column w/server default

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import TIMESTAMP, Column, Integer, String, text
 
 
 class Post(Base):
@@ -8,6 +8,8 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False,
+                        server_default=text('now()'))
 
 
 class User(Base):
